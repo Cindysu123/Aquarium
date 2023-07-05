ab44a8ab5a95480e2990588dc6a364f9b7a56b89
Cindysu123 <cindysu2@illinois.edu>
2023-07-04 19:33:31 -0500

    Add time tracker, expand and collaspes

--- src/components/CenterFishTank.js
+++ src/components/CenterFishTank.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Fish from './Fish';
-import './CenterFishTank.css'
+import './CenterFishTank.css';
 
 const CenterFishTank = ({ tasks }) => {
   const renderTimeScale = () => {
@@ -16,15 +16,45 @@ const CenterFishTank = ({ tasks }) => {
     return timeScale;
   };
 
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const date = new Date();
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const seconds = date.getSeconds().toString().padStart(2, '0');
+      const formattedTime = `${hours}:${minutes}:${seconds}`;
+      setCurrentTime(formattedTime);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const getCurrentTimePosition = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const totalMinutes = hours * 60 + minutes;
+    return `${(totalMinutes / 1440) * 750}%`;
+  };
+
   return (
     <div className="center-fish-tank">
       <div className="timeScaleContent">
         <div className="time-scale">
-            {renderTimeScale()}
+          {renderTimeScale()}
         </div>
+        <div className="current-time">
+          Current Time: {currentTime}
+        </div>
+        {/* <div className="current-time-line" style={{ top: `750%` }} /> */}
+        <div className="current-time-line" style={{ top: getCurrentTimePosition() }} />
       </div>
       <div className="task-container">
-        {tasks.map(task => (
+        {tasks.map((task) => (
           <div key={task.id} className="task">
             <Fish
               name={task.name}
--- src/components/FishTank.js
+++ src/components/FishTank.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Fish from './Fish';
 
-const FishTank = ({ tasks, tankName}) => {
+const FishTank = ({ tasks, tankName }) => {
+  const [expanded, setExpanded] = useState(true);
+
+  const toggleExpansion = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="fish-tank">
-      <h3 className="fish-tank-title">{tankName}</h3>
-      {tasks.map(task => (
-        <Fish
-          key={task.id}
-          name={task.name}
-          source="FishTank"
-          selectedTime={task.time}
-        />
-      ))}
+      <h3 className="fish-tank-title">
+        <button onClick={toggleExpansion}>
+          {expanded ? '-' : '+'}
+        </button>
+        {tankName}
+      </h3>
+      {expanded &&
+        tasks.map((task) => (
+          <Fish
+            key={task.id}
+            name={task.name}
+            source="FishTank"
+            selectedTime={task.time}
+          />
+        ))}
     </div>
   );
 };
 
 export default FishTank;
-
