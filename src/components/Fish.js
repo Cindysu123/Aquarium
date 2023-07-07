2fd868e3816d24b8d8d9352d875e6aab34315b18
Cindysu123 <cindysu2@illinois.edu>
2023-07-07 02:53:17 -0500

    Randomly assign fish

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -7,25 +7,27 @@ import img4 from '../img/fish/fish_4.gif';
 import img5 from '../img/fish/fish_5.gif';
 
 const Fish = ({ name, source, selectedTime, description }) => {
+  // Array of fish images
+  const fishImages = [img1, img2, img3, img4, img5];
 
   const getRandomColor = () => {
     let randomColor = `#473b78`;
-    if(0<{selectedTime}<10){
-      randomColor = `#DFF6FD`
-    } else if(10<={selectedTime}<14){
-      randomColor = `#CBE5FF`
-    } else if(14<={selectedTime}<17){
-      randomColor = `#B8C9E8`
-    } else if(17<={selectedTime}<20){
-      randomColor = `#959CC1`
+    if (0 < selectedTime && selectedTime < 10) {
+      randomColor = `#DFF6FD`;
+    } else if (10 <= selectedTime && selectedTime < 14) {
+      randomColor = `#CBE5FF`;
+    } else if (14 <= selectedTime && selectedTime < 17) {
+      randomColor = `#B8C9E8`;
+    } else if (17 <= selectedTime && selectedTime < 20) {
+      randomColor = `#959CC1`;
     } else {
-      randomColor = `#C1BADD`
+      randomColor = `#C1BADD`;
     }
     return randomColor;
   };
 
-  const topPosition = (45 + (selectedTime-7) * 10.5) + 'vh';
-  const topPosition2 = ((selectedTime-7) * 10.5)/2-20 + 'vh';
+  const topPosition = `${45 + (selectedTime - 7) * 10.5}vh`;
+  const topPosition2 = `${(selectedTime - 7) * 10.5 / 2 - 20}vh`;
 
   if (!document.fishColors) {
     document.fishColors = {};
@@ -37,34 +39,42 @@ const Fish = ({ name, source, selectedTime, description }) => {
 
   const fishStyle = {};
 
-if (source === 'CenterFishTank') {
-  fishStyle.position = 'absolute';
-  fishStyle.top = topPosition;
-  fishStyle.width = '60vh';
-}
+  if (source === 'CenterFishTank') {
+    fishStyle.position = 'absolute';
+    fishStyle.top = topPosition;
+    fishStyle.width = '60vh';
+  }
 
-if (source === 'FishTank') {
-  fishStyle.backgroundColor = document.fishColors[name];
-  fishStyle.width = '30vh';
-  fishStyle.color = '#51516F';
-  fishStyle.padding = '10px';
-  fishStyle.margin = '10px';
-}
+  if (source === 'FishTank') {
+    fishStyle.backgroundColor = document.fishColors[name];
+    fishStyle.width = '30vh';
+    fishStyle.color = '#51516F';
+    fishStyle.padding = '10px';
+    fishStyle.margin = '10px';
+  }
 
-if(source === 'ExpandedFishTank'){
-  fishStyle.position = 'absolute';
-  fishStyle.top = topPosition2;
-  fishStyle.width = '60vh';
-}
+  if (source === 'ExpandedFishTank') {
+    fishStyle.position = 'absolute';
+    fishStyle.top = topPosition2;
+    fishStyle.width = '60vh';
+  }
+
+  // Calculate the index based on the sum of lengths
+  const imageIndex = (name.length + selectedTime.toString().length + description.length) % 5;
+  const selectedImage = fishImages[imageIndex];
 
   return (
     <div
-      className={`fish ${source === 'CenterFishTank' ? 'fish-center' : ''} ${source === 'ExpandedFishTank' ? 'fish-expand' : ''}`}
+      className={`fish ${source === 'CenterFishTank' ? 'fish-center' : ''} ${
+        source === 'ExpandedFishTank' ? 'fish-expand' : ''
+      }`}
       style={fishStyle}
     >
-      <h4 className="fish-text_name">{name} ({selectedTime}:00)</h4>
+      <h4 className="fish-text_name">
+        {name} ({selectedTime}:00)
+      </h4>
       <h4 className="fish-text_description">{description}</h4>
-      <img src={img2} alt="Fish" className='fish-image1' />
+      <img src={selectedImage} alt="Fish" className="fish-image1" />
     </div>
   );
 };
